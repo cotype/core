6b95a9e2031b7cb79f4125597054db7859f9bed7
Hannes Diercks <github@xiphe.net>
2019-05-21 12:48:53 +0200

    chore(cypress): 🔨 workaround for cypress worker error
    ref https://github.com/parcel-bundler/parcel/issues/2838
    ref https://github.com/cypress-io/cypress-parcel-preprocessor/pull/40

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,35 @@
-const cyParcel = require("cypress-parcel-preprocessor");
+const cyParcel = require("@xiphe/cypress-parcel-preprocessor");
+
+const RETRY = Symbol("RETRY");
+const MAX_RETRIES = 10;
+
+function wait(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+async function justFrigginPreprocessPlease(ev) {
+  try {
+    return await cyParcel(ev);
+  } catch (err) {
+    switch (ev[RETRY]) {
+      case undefined:
+        ev[RETRY] = 1;
+        break;
+      case MAX_RETRIES:
+        throw err;
+      default:
+        await wait(ev[RETRY]);
+        ev[RETRY] += 1;
+    }
+    console.log(
+      `Parcel threw an ERROR: Retrying(${ev[RETRY]} of ${MAX_RETRIES})...`
+    );
+  }
+  return justFrigginPreprocessPlease(ev);
+}
 
 module.exports = (on, config) => {
-  on("file:preprocessor", async ev => {
-    try {
-      return await cyParcel(ev);
-    } catch (err) {
-      console.log("ERROR. Retrying ...");
-    }
-    return cyParcel(ev);
-  });
+  on("file:preprocessor", justFrigginPreprocessPlease);
 };
