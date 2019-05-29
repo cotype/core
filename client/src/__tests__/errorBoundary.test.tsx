faf7248e9bb6b88bdd2dc1e0e455b023403ca976
= <dlehmhus@cellular.de>
2019-05-29 16:18:06 +0200

    test(client): add test-case for error boundary

---
+++ client/src/__tests__/errorBoundary.test.tsx
@@ -0,0 +1,30 @@
+jest.mock("../Header", () => ({
+  HEIGHT: 50
+}));
+jest.mock("../basePath", () => "");
+
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import React from "react";
+import { render } from "react-testing-library";
+import ErrorBoundary from "../ErrorBoundary";
+
+beforeEach(() => {
+  // when the error's thrown a bunch of console.errors are called even though
+  // the error boundary handles the error. This makes the test output noisy,
+  // so we'll mock out console.error
+  jest.spyOn(console, "error").mockImplementation(() => undefined);
+});
+
+test("catches error and renders that there was a problem", async () => {
+  const Foo = () => {
+    throw new Error("NOPE");
+  };
+  const { container } = render(
+    <ErrorBoundary>
+      <Foo />
+    </ErrorBoundary>
+  );
+
+  expect(container).toHaveTextContent(/ups, something went wrong/i);
+});
