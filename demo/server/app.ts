bba1250faba23a2b88f2ad2d2b8ee02f868991dc
Hannes Diercks <github@xiphe.net>
2019-06-06 17:49:49 +0200

    chore(demo): rebuild reinit logic
    dont use express.listen but wrap in custom http server
    
    in order to support complete shutdown of the app
    while re-init route is served

---
+++ demo/server/app.ts
@@ -0,0 +1,108 @@
+import * as Cotype from "../../typings";
+import {
+  init,
+  knexAdapter,
+  Opts,
+  FsStorage,
+  clientMiddleware as prodClientMiddleware
+} from "../../src";
+import * as path from "path";
+import { models } from "../models";
+import { navigation } from "../navigation.json";
+import devClientMiddleware from "./devClientMiddleware";
+import { Express } from "express";
+import ContentPersistence from "../../src/persistence/ContentPersistence";
+import LocalThumbnailProvider from "@cotype/local-thumbnail-provider";
+
+const uploadDir = path.resolve(__dirname, "..", "uploads");
+const storage = new FsStorage(uploadDir);
+
+function getClient(url: string) {
+  if (url.match(/^postgres/)) return "pg";
+  if (url.match(/^mysql/)) return "mysql";
+  throw new Error("Unsupported connection string: " + url);
+}
+
+export function getKnexConfig() {
+  if (!process.env.DB || process.env.NODE_ENV === "test") {
+    return {
+      client: "sqlite3",
+      connection: {
+        filename: path.join(__dirname, "..", `${process.env.NODE_ENV}_db`)
+      },
+      useNullAsDefault: true
+    };
+  }
+  return {
+    connection: process.env.DB,
+    client: getClient(process.env.DB)
+  };
+}
+
+const defaultConfig: Opts = {
+  models: models as Cotype.ModelOpts[],
+  storage,
+  navigation: navigation as Cotype.NavigationOpts[],
+  thumbnailProvider: new LocalThumbnailProvider(storage),
+  persistenceAdapter: knexAdapter(getKnexConfig()),
+  customSetup: (app: Express, persitence: ContentPersistence) => {
+    app.use("/crazyCustomRoute", async (req, res) => {
+      res.json({ hello: "world" });
+    });
+  },
+  contentHooks: {
+    preHooks: {
+      onSave: async (_, data) => {
+        return { ...data, hidden: [{ key: 0, value: "hidden value 22" }] };
+      }
+    },
+    postHooks: {
+      onCreate: async () => {
+        console.info("onCreate");
+      },
+      onSave: async () => {
+        console.info("onSave");
+      },
+      onDelete: async () => {
+        console.info("onDelete");
+      },
+      onPublish: async () => {
+        console.info("onPublish");
+      },
+      onUnpublish: async () => {
+        console.info("onUnpublish");
+      },
+      onSchedule: async () => {
+        console.info("onSchedule");
+      }
+    }
+  }
+};
+
+function getClientMiddleware({ clientMiddleware, basePath }: Partial<Opts>) {
+  if (clientMiddleware) {
+    return clientMiddleware;
+  }
+
+  return process.env.NODE_ENV === "production" ||
+    process.env.CLIENT_MIDDLEWARE === "production"
+    ? [prodClientMiddleware]
+    : [devClientMiddleware(basePath)];
+}
+
+export default async function app(customConfig?: Partial<Opts>) {
+  const preConfig = {
+    ...defaultConfig,
+    ...customConfig
+  };
+
+  const config = {
+    ...preConfig,
+    clientMiddleware: getClientMiddleware(preConfig)
+  };
+
+  return {
+    ...(await init(config)),
+    config
+  };
+}
--- demo/server/server.ts
+++ demo/server/server.ts
@@ -1,118 +1,27 @@
-import * as Cotype from "../../typings";
-import {
-  init as initCotype,
-  knexAdapter,
-  Opts,
-  FsStorage,
-  clientMiddleware as prodClientMiddleware
-} from "../../src";
-import * as path from "path";
-import { models } from "../models";
-import { navigation } from "../navigation.json";
-import devClientMiddleware from "./devClientMiddleware";
-import reInitMiddleware from "./reInitMiddleware";
-import { Server } from "http";
-import { Express } from "express";
-import ContentPersistence from "../../src/persistence/ContentPersistence";
-import LocalThumbnailProvider from "@cotype/local-thumbnail-provider";
+import { createServer } from "http";
+import getApp from "./app";
+import { log } from "../../src";
+import reinit from "./reinit";
 
-const uploadDir = path.resolve(__dirname, "..", "uploads");
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
-function getClient(url: string) {
-  if (url.match(/^postgres/)) return "pg";
-  if (url.match(/^mysql/)) return "mysql";
-  throw new Error("Unsupported connection string: " + url);
-}
+let appP = getApp();
 
-const PORT = process.env.PORT || 4000;
-
-function getKnexConfig() {
-  if (!process.env.DB || process.env.NODE_ENV === "test") {
-    return {
-      client: "sqlite3",
-      connection: {
-        filename: path.join(__dirname, "..", `${process.env.NODE_ENV}_db`)
-      },
-      useNullAsDefault: true
-    };
-  }
-  return {
-    connection: process.env.DB,
-    client: getClient(process.env.DB)
-  };
-}
-
-export type InitState = {
-  server?: Server;
-};
-
-async function init(initialConfig: Opts) {
-  const state: InitState = {};
-  const clientMiddleware =
-    process.env.NODE_ENV === "production" ||
-    process.env.CLIENT_MIDDLEWARE === "production"
-      ? [prodClientMiddleware]
-      : [devClientMiddleware(initialConfig.basePath)];
-
-  const startServer = async (config: Opts) => {
-    const { app } = await initCotype({ ...config, clientMiddleware });
-    state.server = app.listen({ port: PORT });
-  };
-
-  if (process.env.NODE_ENV === "test") {
-    clientMiddleware.unshift(
-      reInitMiddleware(initialConfig, startServer, getKnexConfig, state)
-    );
-  }
-
-  await startServer(initialConfig);
-}
-
-const storage = new FsStorage(uploadDir);
-
-init({
-  models: models as Cotype.ModelOpts[],
-  storage,
-  navigation: navigation as Cotype.NavigationOpts[],
-  thumbnailProvider: new LocalThumbnailProvider(storage),
-  persistenceAdapter: knexAdapter(getKnexConfig()),
-  customSetup: (app: Express, persitence: ContentPersistence) => {
-    app.use("/crazyCustomRoute", async (req, res) => {
-      res.json({ hello: "world" });
-    });
-  },
-  contentHooks: {
-    preHooks: {
-      onSave: async (model, data) => {
-        return { ...data, hidden: [{ key: 0, value: "hidden value 22" }] };
-      }
-    },
-    postHooks: {
-      onCreate: async (model, data) => {
-        console.info("onCreate");
-      },
-      onSave: async (model, data) => {
-        console.info("onSave");
-      },
-      onDelete: async (model, data) => {
-        console.info("onDelete");
-      },
-      onPublish: async (model, data) => {
-        console.info("onPublish");
-      },
-      onUnpublish: async (model, data) => {
-        console.info("onUnpublish");
-      },
-      onSchedule: async (model, data) => {
-        console.info("onSchedule");
-      }
+createServer(async (req, res) => {
+  try {
+    if (
+      process.env.NODE_ENV === "test" &&
+      req.method === "POST" &&
+      req.url === "/admin/__reinit"
+    ) {
+      appP = reinit(req, appP).then(getApp);
+      res.statusCode = 204;
+      res.end();
+    } else {
+      (await appP).app(req, res);
     }
-  }
-})
-  .then(() => {
-    console.info(`🚀 Server ready at http://localhost:${PORT}`);
-  })
-  .catch(error => {
-    console.error(error);
+  } catch (err) {
+    log.error(err);
     process.exit(1);
-  });
+  }
+}).listen(port, () => log.info(`Server ready at http://localhost:${port}`));
