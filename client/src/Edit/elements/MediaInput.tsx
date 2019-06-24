1634d79922d9d6342c8a7fce5d3be68af6c76d2a
Marius Ostheimer <mail@mostheimer.de>
2019-06-24 11:25:53 +0200

    fix(client): Show required mimeType

--- client/src/Edit/elements/MediaInput.tsx
+++ client/src/Edit/elements/MediaInput.tsx
@@ -101,7 +101,7 @@ export default class MediaInput extends Component<Props, State> {
   }
 
   static getHint(model) {
-    const { minHeight, minWidth, maxHeight, maxWidth, maxSize } = model;
+    const { minHeight, minWidth, maxHeight, maxWidth, maxSize, mimeType } = model;
     const str: string[] = [];
 
     if (minHeight) {
@@ -118,6 +118,9 @@ export default class MediaInput extends Component<Props, State> {
     }
     if (maxSize) {
       str.push("Max Größe: " + sizeFormat(maxSize, 2));
+    }
+    if (mimeType) {
+      str.push("Erlaubtes Dateiformat: "+mimeType);
     }
     if (str.length > 0) {
       return `(${str.join(", ")})`;
