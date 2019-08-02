405612348f816806cbd1dac8a3005ce04af96de3
Marius Ostheimer <mostheimer@cellular.de>
2019-08-02 14:56:21 +0200

    fix(client): Fix Type

--- client/src/Media/UploadZone.tsx
+++ client/src/Media/UploadZone.tsx
@@ -70,7 +70,7 @@ export default function UploadZone({
             error,
             files
           }),
-        [render, progress, files, onFiles, onUpload, done, response, error]
+        [render, progress, files, onFiles, done, response, error]
       )}
     </div>
   );
