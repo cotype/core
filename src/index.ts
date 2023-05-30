05b61014fff652f5f285508adc704913fc1f4017
Hanan Hamza <hanan.hamza@ffw.com>
2023-05-30 16:30:13 +0200

    fix: types import

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ type SessionOpts = CookieSessionInterfaces.CookieSessionOptions;
 
 export { Persistence } from "./persistence";
 export { default as knexAdapter } from "./persistence/adapter/knex";
-export type { default as Cotype } from "../typings";
+export type * from "../typings";
 export { default as FsStorage } from "./media/storage/FsStorage";
 
 export * from "./utils";
